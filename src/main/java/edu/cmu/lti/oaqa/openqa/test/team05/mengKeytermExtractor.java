d8d1a6c8edba8c1dcf6f9bcca99a07ce074b448e
mpzhu <phyllis0618@gmail.cm>
2012-11-05 18:19:45 -0500

    1

---
+++ src/main/java/edu/cmu/lti/oaqa/openqa/test/team05/mengKeytermExtractor.java
@@ -0,0 +1,16 @@
+package edu.cmu.lti.oaqa.openqa.test.team05;
+
+import java.util.List;
+
+import edu.cmu.lti.oaqa.cse.basephase.keyterm.AbstractKeytermExtractor;
+import edu.cmu.lti.oaqa.framework.data.Keyterm;
+
+public class mengKeytermExtractor extends AbstractKeytermExtractor {
+
+  @Override
+  protected List<Keyterm> getKeyterms(String arg0) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}

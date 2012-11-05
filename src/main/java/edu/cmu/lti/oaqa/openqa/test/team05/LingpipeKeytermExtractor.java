25f2a8a81508a7baea5bc9457309b8abbf65541d
Yuliang Yin <yyl0827@gmail.com>
2012-11-05 16:48:13 -0500

    Working system

---
+++ src/main/java/edu/cmu/lti/oaqa/openqa/test/team05/LingpipeKeytermExtractor.java
@@ -0,0 +1,76 @@
+package edu.cmu.lti.oaqa.openqa.test.team05;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.uima.resource.ResourceAccessException;
+
+import com.aliasi.chunk.Chunk;
+import com.aliasi.chunk.ConfidenceChunker;
+import com.aliasi.util.AbstractExternalizable;
+
+import edu.cmu.lti.oaqa.cse.basephase.keyterm.AbstractKeytermExtractor;
+import edu.cmu.lti.oaqa.framework.data.Keyterm;
+
+public class LingpipeKeytermExtractor extends AbstractKeytermExtractor {
+
+  /**
+   * The maximum number of chunks that the chunker will return in order of confidence.
+   */
+  private static final int MAX_N_BEST_CHUNKS = 8;
+  
+  /**
+   * The confidence threshold to help us decide whether we should consider a chunk as
+   * a true gene tag.
+   */
+  private static final double CONFIDENCE_THRESHOLD = 0.5;
+
+  /**
+   * A confidence chunker that provided by LingPipe
+   */
+  private static ConfidenceChunker chunker;
+  
+  @Override
+  protected List<Keyterm> getKeyterms(String text) {
+    
+    if (chunker == null) {
+      try {
+        chunker = (ConfidenceChunker) AbstractExternalizable.readObject(new File("src/main/resources/model/ne-en-bio-genetag.HmmChunker"));
+      } catch (IOException e) {
+        e.printStackTrace();
+      } catch (ClassNotFoundException e) {
+        e.printStackTrace();
+      }
+    }
+    
+    
+    List<Keyterm> keyterms = new ArrayList<Keyterm>();
+    
+    char[] cs = text.toCharArray();
+    
+    // Extract gene mentions
+    Iterator<Chunk> it = chunker.nBestChunks(cs, 0, cs.length, MAX_N_BEST_CHUNKS);
+
+    while (it.hasNext()) {
+      Chunk chunk = it.next();
+
+      // Get begin and end
+      int begin = chunk.start();
+      int end = chunk.end();
+      // Get confidence
+      double conf = Math.pow(2.0, chunk.score());
+
+      // Ignore confidence less than 0.5
+      if (conf < CONFIDENCE_THRESHOLD)
+        continue;
+      
+      keyterms.add(new Keyterm(text.substring(begin, end)));
+    }
+    
+    return keyterms;
+  }
+
+}
